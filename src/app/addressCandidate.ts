4015f4aeac7f90d4ff4e41e7b24087e84eed6f0c
jcc <john.c.cartwright@comcast.net>
2019-09-14 14:20:38 -0600

    xfer angular project out of master branch

--- src/app/addressCandidate.ts
+++
@@ -1,7 +0,0 @@
-export interface AddressCandidate {
-    address: string;
-    location: Map<string, number>;
-    score: number;
-    attributes: Map<string, string>;
-    extent: Map<string, number>;
-}
